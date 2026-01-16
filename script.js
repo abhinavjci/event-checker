b3f35d34522eedb5cd9b96e35f9702b1bcae843b
abhinavjci <jsin68@jci.com>
2026-01-16 19:47:29 +0000

    Regular events added

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let errorCounts = 0;
 const ignoreOperativeFor = ["Humber IT", "Support Services", "GE", "Bio-Medical", "Security", "Bal John", "Central Dispatch", "Mike Orrico", "Clintar", "Todd Haynes", "Yondy Lam", "JCI Fire & Security"];
 
 // Services to ignore when no Operative is assigned to the event
-const serviceIgnoreList = ["General Facility"];
+const serviceIgnoreList = ["General Facility", "Elevators"];
 
 // Remarks that will help to ignore errors when no Operative is assigned to regular events
 const remarksIgnoreList = ["Jeff", "Duplicate", "Modern Niagara"];
@@ -23,23 +23,111 @@ table.querySelectorAll("tr").forEach((row, index) => {
     // If something, fetch all the data
     const cells = row.querySelectorAll("td");
 
-    // Now, we have the element; Let's store the values of each cell
-    const event = cells[0].textContent.trim();
-    const status = cells[1].textContent.trim();
-    const location = cells[2].textContent.trim();
-    const service = cells[3].textContent.trim();
-    const workType = cells[4].textContent.trim();
-    const contractor = cells[5].textContent.trim();
-    const operative = cells[6].textContent.trim();
-    const instructions = cells[7].textContent.trim();
-    const remarks = cells[8].textContent.trim();
-    const reportedBy = cells[9].textContent.trim();
+    // Event Elements
+    const eventCell = cells[0];
+    const statusCell = cells[1];
+    const locationCell = cells[2];
+    const serviceCell = cells[3];
+    const workTypeCell = cells[4];
+    const contractorCell = cells[5];
+    const operativeCell = cells[6];
+    const instructionsCell = cells[7];
+    const remarksCell = cells[8];
+    const reportedByCell = cells[9];
 
+    // Now, we have the element; Let's store the values of each cell
+    const event = eventCell.textContent.trim();
+    const status = statusCell.textContent.trim();
+    const location = locationCell.textContent.trim();
+    const service = serviceCell.textContent.trim();
+    const workType = workTypeCell.textContent.trim();
+    const contractor = contractorCell.textContent.trim();
+    const operative = operativeCell.textContent.trim();
+    const instructions = instructionsCell.textContent.trim();
+    const remarks = remarksCell.textContent.trim();
+    const reportedBy = reportedByCell.textContent.trim();
+
+    // Logic 1: Checking if required cells are empty (Remarks)
+    if (
+        remarks === "" ||
+        remarks === "\u00A0"
+    ) { mark(remarksCell, "error"); }
+
+    // Logic 2: Checking the missing operative with the condition (was even required or not)
+    else if (
+        operative === "\u00A0" ||
+        operative === ""
+    ) {
+        if (
+            // Contractor have operative
+            !ignoreOperativeFor.includes(contractor) &&
+            // Service have operative
+            !serviceIgnoreList.includes(service) &&
+            // Remarks doesn't satify that operative was informed
+            !remarksIgnoreList.some(word => remarks.includes(word))
+            // Then it's a Flag
+        ) { mark(operativeCell, "error"); }
+    }
+
+    // Logic 3: Temperature events
+    // 1. Template is Right
+    // 2. Instructions Matches with Service
+    if (
+        service == "Room Too Cold" || 
+        service == "Room Too Hot"
+    ) {
+        // Template is Right
+        if (remarks.includes("RDS")) { 
+            mark(remarksCell); 
+        }
+        
+        else { mark(remarksCell, "error"); }
+    }
+
+    if (
+        service == "Room Too Cold" || 
+        service == "Room Too Hot"
+    ) {
+        // Instructions mathces with service
+        // Service matches to Instructions
+        // Too Cold
+        if ( 
+            service == "Room Too Cold" && 
+            instructions.includes("cold") 
+        ) { 
+            mark(serviceCell); 
+            mark(instructionsCell); 
+        }
+
+        // Too Hot
+        else if (
+            service == "Room Too Hot" &&
+            instructions.includes("hot")
+        ) {
+            mark(serviceCell);
+            mark(instructionsCell);
+        }
+
+        else { 
+            mark(serviceCell, "error");
+            mark(instructionsCell, "error");
+         }
+    }
 
     // And at the end, let's see how many totalEvents we have
     totalEvents++;
 });
 
+// Functions
+// Marker for errors or correct values
+function mark(element, type="non-error") {
+    let color = "green";
+    if (type == "error") { color = "red"; errorCounts++ }
+    element.style.backgroundColor = color;
+    element.style.color = "white";
+}
+
+// Showing the important information at the end
 alert(`Hey Man, below are the stats
-       Total Events are: ${totalEvents}
-       Total Warnings are: ${errorCounts}`)
+       > Total Events are: ${totalEvents}
+       > Total Warnings are: ${errorCounts}`);
